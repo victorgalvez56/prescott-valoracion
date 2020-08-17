5925b8e298429c23fe4a6ba6bb772d54d8c3e9f1
unknown <victor.galvez56@gmail.com>
2020-08-17 17:45:34 -0500

    ga

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'root',
 	'password' => '',
 	'database' => 'db_valoracion',
-	'dbdriver' => 'mysqli',
+	'dbdriver' => 'mysql',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
