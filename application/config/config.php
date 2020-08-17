fca4e0e5555bffe23b6f032a247a812ae4f881a3
unknown <victor.galvez56@gmail.com>
2020-08-17 17:00:00 -0500

    ga

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/prescott-valoracion/';
+$config['base_url'] = 'http://206.189.234.29:8000/prescott-valoracion';
 
 /*
 |--------------------------------------------------------------------------
